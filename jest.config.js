8f1e3889c7f9088e71fed77b0d717d0503f44f55
Tim Arney <tim@line37.com>
2019-09-06 15:17:20 -0400

    modify coverage

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 90,
-      lines: 90,
-      statements: 90
+      branches: 75,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
   }
 };
