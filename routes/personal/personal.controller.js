fee7e3d40490a8caaa0a90a14528bb8e347b5bd2
Tim Arney <tim@line37.com>
2019-09-09 09:24:20 -0400

    - adds routeUtils to simplify common imports - adds getDefaultMiddleware to simplify common middleware imports

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -1,44 +1,18 @@
 const path = require("path");
-const { checkSchema } = require("express-validator");
-const {
-  doRedirect,
-  checkErrors,
-  checkNonce,
-  generateNonce,
-  getRouteByName,
-  addViewPath,
-  getSessionData,
-  getFlashMessage
-} = require("./../../utils");
+const { routeUtils } = require("./../../utils");
 const { Schema } = require("./schema.js");
 
 module.exports = app => {
   const name = "personal";
-  const route = getRouteByName(name);
+  const route = routeUtils.getRouteByName(name);
 
-  addViewPath(app, path.join(__dirname, "./"));
+  routeUtils.addViewPath(app, path.join(__dirname, "./"));
 
-  app.get(route.path, (req, res) => {
-    const params = {
-      data: getSessionData(req),
-      name,
-      nonce: generateNonce()
-    };
-
-    const errors = getFlashMessage(req);
-
-    if (errors) {
-      params.errors = errors;
-    }
-
-    res.render(name, params);
-  });
-
-  app.post(
-    route.path,
-    checkNonce,
-    checkSchema(Schema),
-    checkErrors(name),
-    doRedirect
-  );
+  app
+    .get(route.path, (req, res) => {
+      res.render(name, routeUtils.getViewData(req, name));
+    })
+    .post(route.path, [
+      ...routeUtils.getDefaultMiddleware({ schema: Schema, name: name })
+    ]);
 };
--- utils/index.spec.js
+++ utils/index.spec.js
@@ -2,19 +2,8 @@
 
 const {
   isValidDate,
-  getRouteWithIndexByName,
-  getRouteByName,
-  getPreviousRoute,
-  getNextRoute,
-  isEmptyObject,
-  doRedirect
+  isEmptyObject
 } = require("./index");
-
-const testRoutes = [
-  { name: "start", path: "/start" },
-  { name: "personal", path: "/personal" },
-  { name: "confirmation", path: "/confirmation" }
-];
 
 describe("Has valid date", () => {
   test("returns true for valid date", () => {
@@ -34,37 +23,7 @@ describe("Has valid date", () => {
   });
 });
 
-describe("Routes", () => {
-  test("finds route index by name", () => {
-    const obj = getRouteWithIndexByName("personal", testRoutes);
-    expect(obj.index).toEqual(1);
-  });
 
-  test("finds route path by name", () => {
-    const obj = getRouteByName("personal", testRoutes);
-    expect(obj.path).toEqual("/personal");
-  });
-
-  test("return false for previous route that doesn't exist", () => {
-    const obj = getPreviousRoute("start", testRoutes);
-    expect(obj.path).toEqual(false);
-  });
-
-  test("finds previous route path by name", () => {
-    const obj = getPreviousRoute("personal", testRoutes);
-    expect(obj.path).toEqual("/start");
-  });
-
-  test("return false for next route that doesn't exist", () => {
-    const obj = getNextRoute("confirmation", testRoutes);
-    expect(obj.path).toEqual(false);
-  });
-
-  test("finds next route path by name", () => {
-    const obj = getNextRoute("personal", testRoutes);
-    expect(obj.path).toEqual("/confirmation");
-  });
-});
 
 describe("Is empty Object", () => {
   test("returns true is the object is empty", () => {
@@ -76,40 +35,4 @@ describe("Is empty Object", () => {
     const result = isEmptyObject({ name: "your name" });
     expect(result).toEqual(false);
   });
-});
-
-test("getPreviousRoute will throw an error when missing params", () => {
-  expect(() => {
-    getPreviousRoute();
-  }).toThrow();
-});
-
-test("getNextRoute will throw an error when missing params", () => {
-  expect(() => {
-    getNextRoute();
-  }).toThrow();
-});
-
-test("Calls next if json is requested", () => {
-  const req = { body: { json: true } };
-  const next = jest.fn();
-  doRedirect(req, {}, next);
-  expect(next.mock.calls.length).toBe(1);
-});
-
-test("Calls redirect if it finds the next route", () => {
-  const req = { body: { name: "start" } };
-  const next = jest.fn();
-  const redirectMock = jest.fn();
-  const res = {
-    query: {},
-    headers: {},
-    data: null,
-    redirect: () => {
-      redirectMock();
-    }
-  };
-  doRedirect(req, res, next);
-  expect(next.mock.calls.length).toBe(0);
-  expect(redirectMock.mock.calls.length).toBe(1);
 });
