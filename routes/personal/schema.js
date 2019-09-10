3cd6af886ee47230766e2c1b4422f5e4d6302c74
Tim Arney <tim@line37.com>
2019-09-09 20:05:37 -0400

    - Fix checkbox validation - Fixes textArea value - Fixes checkbox could only have a single value - Resets checkbox user selects no notifications

--- routes/personal/schema.js
+++ routes/personal/schema.js
@@ -34,16 +34,23 @@ const Schema = {
       options: [["Yes", "No"]]
     }
   },
-
   notify_type: {
     custom: {
       options: (value, { req }) => {
-        console.log("val",value)
-        //return isValidDate(value);
+        const send_notifications = req.body.send_notifications;
+        if (send_notifications && send_notifications === "Yes") {
+          if (typeof value === "undefined") {
+            return false;
+          }
+        } else {
+          req.body.notify_type = undefined;
+        }
+
+        return true;
       },
       errorMessage: "errors.notify_type"
     }
-  },
+  }
 };
 
 module.exports = {
