dfe6dd932f9431748f4ef8d1bb64810b17532764
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-09-11 15:18:45 -0400

    switch replacement tokens to [[ ]] for compatibility with nunjucks

--- bin/route/[[sample]]/[[sample]].controller.js
+++ bin/route/[[sample]]/[[sample]].controller.js
@@ -3,14 +3,14 @@ const { getNextRoute, routeUtils } = require('./../../utils')
 const { Schema } = require('./schema.js')
 
 module.exports = app => {
-  const name = '{{sample}}'
+  const name = '[[sample]]'
   const route = routeUtils.getRouteByName(name)
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
 
   app
     .get(route.path, (req, res) => {
-      res.render(name, {... routeUtils.getViewData(req, {}), nextRoute: getNextRoute(name).path})
+      res.render(name, { ...routeUtils.getViewData(req, {}), nextRoute: getNextRoute(name).path })
     })
     .post(route.path, [
       ...routeUtils.getDefaultMiddleware({ schema: Schema, name: name }),
