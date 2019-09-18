f9c1c1694a3f4c08aff2420c9a43ec4858fee882
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-09-18 13:23:15 -0400

    add csrf protection to routes

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -2,6 +2,9 @@ const path = require('path')
 const { routeUtils } = require('./../../utils')
 const { Schema } = require('./schema.js')
 
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+
 module.exports = app => {
   const name = 'personal'
   const route = routeUtils.getRouteByName(name)
@@ -9,11 +12,11 @@ module.exports = app => {
   routeUtils.addViewPath(app, path.join(__dirname, './'))
 
   app
-    .get(route.path, (req, res) => {
+    .get(route.path, csrfProtection, (req, res) => {
       const jsFiles = ['js/file-input.js']
       res.render(name, { ...routeUtils.getViewData(req), jsFiles })
     })
-    .post(route.path, [
+    .post(route.path, csrfProtection, [
       ...routeUtils.getDefaultMiddleware({ schema: Schema, name: name }),
     ])
 }
