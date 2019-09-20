648b0d1e92ca18f1b6369689597a6fe52b51ccca
Tim Arney <tim@line37.com>
2019-09-20 11:10:57 -0400

    add tests for start controller

---
+++ routes/start/start.controller.spec.js
@@ -0,0 +1,37 @@
+const request = require('supertest')
+const app = require('../../app.js')
+const { getRouteByName } = require('../../utils/route.helpers')
+const cheerio = require('cheerio')
+
+function countScriptTags(res) {
+  var $ = cheerio.load(res.text)
+  return $('script').length
+}
+
+const mockFn = jest
+  .fn((req, routePath, jsPath = null) => 'default')
+  .mockImplementationOnce(() => 'https://digital.canada.ca')
+  .mockImplementationOnce(() => false)
+
+jest.mock('../../utils/load.helpers.js', () => ({
+  getClientJs: () => {
+    return mockFn()
+  },
+}))
+
+test('Can send get request to start route and have js src set', async () => {
+  const route = getRouteByName('start')
+  const response = await request(app).get(route.path)
+  expect(response.statusCode).toBe(200)
+  expect(response.text).toContain('digital.canada.ca')
+})
+
+test('Can send get request to start route and have empty js src', async () => {
+  const route = getRouteByName('start')
+  const response = await request(app).get(route.path)
+  expect(response.statusCode).toBe(200)
+  // call to getClientJs should return false
+  // which means we should have X number of script tags
+  // i.e. whatever the amount is in the base view
+  expect(countScriptTags(response)).toBe(2)
+})
