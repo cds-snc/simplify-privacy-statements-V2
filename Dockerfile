0053195eaf4f29caf26ae93b5ebd7071e44e4f54
Stephen McMurtry <smcmurtry@gmail.com>
2019-09-24 11:53:49 -0600

    Got the docx download feature working, changed docker image (#20)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM node:10-alpine
+FROM pandoc/core
 LABEL maintainer="-"
+
+RUN apk add --update nodejs nodejs-npm
 
 ARG GITHUB_SHA_ARG
 
@@ -10,5 +12,8 @@ WORKDIR /src
 RUN npm install --quiet --production
 
 EXPOSE 3000
+
+# heroku needs this
+CMD [ "npm", "start" ]
 
 ENTRYPOINT [ "npm", "start" ]
--- routes/agreement-1/agreement-1.controller.js
+++ routes/agreement-1/agreement-1.controller.js
@@ -1,6 +1,19 @@
 const path = require('path')
 const { getNextRoute, routeUtils } = require('./../../utils')
 const { Schema } = require('./schema.js')
+var nodePandoc = require("node-pandoc");
+
+var callback = (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("done conversion");
+  }
+};
+
+function getRandomString() {
+  return Math.random().toString().split(".")[1].slice(0,8);
+}
 
 module.exports = app => {
   const name = 'agreement-1'
@@ -8,9 +21,22 @@ module.exports = app => {
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
 
-  app
+    app
     .get(route.path, (req, res) => {
-      res.render(name, { ...routeUtils.getViewData(req, {}), nextRoute: getNextRoute(name).path })
+      var randomString = getRandomString()
+      var docxFilename = "agreement-" + randomString + ".docx"
+
+      res.render(name, { 
+        ...routeUtils.getViewData(req, {}), 
+        nextRoute: getNextRoute(name).path,
+        docxFilename: docxFilename,
+      }, function(err, html) {
+        if(err) {
+          console.log(err)
+        }
+        nodePandoc(html, "-f html -t docx -o public/documents/" + docxFilename, callback)
+        res.send(html);
+      })
     })
     .post(route.path, [
       ...routeUtils.getDefaultMiddleware({ schema: Schema, name: name }),
