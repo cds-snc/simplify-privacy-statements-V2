45833e4af4335788f95ac1ec9e510d12b405905c
Steve Astels <sastels@gmail.com>
2019-09-25 16:21:11 -0400

    set initial toggle state correctly

--- public/js/toggle-area.js
+++ public/js/toggle-area.js
@@ -9,12 +9,13 @@ const toggleArea = event => {
   // - future {attributes : {data-something: "something"}, values: { '1':'Yes','2':'No'}}
   const toggleState = event.target.getAttribute('data-index')
   const name = event.target.name
+  const checked = event.target.checked
 
   // look for an element with that name of the target + -toggled
   // i.e. div you want to show or hide
   const target = document.querySelector(`.${name}-toggled`)
 
-  if (!target) return
+  if (!checked || !target) return
 
   if (toggleState === 'on') {
     target.style.display = 'block'
@@ -27,5 +28,6 @@ const toggleArea = event => {
 const toggles = document.querySelectorAll('.toggle-area input')
 
 toggles.forEach(el => {
+  toggleArea({ target: el })
   el.addEventListener('click', toggleArea)
 })
