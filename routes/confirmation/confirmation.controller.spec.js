bd4671cd821f88a555f2e3e040cc7c98ed059754
smcmurtry <smcmurtry@gmail.com>
2019-09-26 10:06:54 -0600

    removed unused paths

--- routes/confirmation/confirmation.controller.spec.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-const app = require('../../app.js')
-const { getRouteByName } = require('../../utils/route.helpers')
-
-test('Confirmation page loads', async () => {
-  const route = getRouteByName('confirmation')
-  const response = await request(app).get(route.path)
-  expect(response.statusCode).toBe(200)
-})
