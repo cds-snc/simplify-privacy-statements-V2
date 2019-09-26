10a0a6727e19fc8da8104bb6abae8333521b0a68
smcmurtry <smcmurtry@gmail.com>
2019-09-26 11:03:20 -0600

    mocking node-pandoc

--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../../app.js')
 const { getRouteByName } = require('../../utils/route.helpers')
-
+jest.mock("node-pandoc", () => () => null);
 
 test('Can send get request agreement-1 route ', async () => {
   const route = getRouteByName('agreement-1')
