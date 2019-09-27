d327a9cf855d70837daca39af6a0a5acc632a407
Stephen McMurtry <smcmurtry@gmail.com>
2019-09-27 07:41:59 -0600

    Removed unused routes (#42)

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,9 +3,6 @@ const routes = [
   { name: 'landing-page', path: '/' },
   { name: 'questions-1', path: '/questions-1' },
   { name: 'agreement-1', path: '/agreement-1' },
-  { name: 'confirmation', path: '/confirmation' },
-  { name: 'participant-consent-form', path: '/participant-consent-form' },
-  { name: 'personal', path: '/personal' },
 ]
 
 // note: you can define and export a custom configRoutes function here
