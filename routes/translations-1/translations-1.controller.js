f4e322027feaa4077a63664bae76dddd42e991d1
smcmurtry <smcmurtry@gmail.com>
2019-10-03 11:10:01 -0600

    fixed lint warning

--- routes/translations-1/translations-1.controller.js
+++ routes/translations-1/translations-1.controller.js
@@ -7,14 +7,13 @@ module.exports = app => {
   const route = routeUtils.getRouteByName(name)
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
-  const otherLanguageDict = {en: "fr", fr: "en"}
   app
     .get(route.path, (req, res) => {
-      const language = i18n.getLocale(req)
+      const language = i18n.getLocale(req) === "en" ? "en" : "fr"
+      const otherLanguage = i18n.getLocale(req) === "en" ? "fr" : "en"
       const jsFiles = ['js/toggle-area.js']
       res.render(name, {
-        ...routeUtils.getViewData(req, {otherLanguage: "_fr", language: "_en"}),
-        ...routeUtils.getViewData(req, {otherLanguage: `_${otherLanguageDict[language]}`, language: `_${language}`}),
+        ...routeUtils.getViewData(req, {otherLanguage: `_${otherLanguage}`, language: `_${language}`}),
         jsFiles,
       })
     })
