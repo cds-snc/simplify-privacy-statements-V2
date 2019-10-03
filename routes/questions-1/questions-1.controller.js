3a2849bc1339c8c951b04f74bf3b6c7332ef912f
smcmurtry <smcmurtry@gmail.com>
2019-10-03 11:02:35 -0600

    fixed the translations page

--- routes/questions-1/questions-1.controller.js
+++ routes/questions-1/questions-1.controller.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { routeUtils } = require('./../../utils')
 const { Schema } = require('./schema.js')
+const i18n = require('i18n')
 
 module.exports = app => {
   const name = 'questions-1'
@@ -12,7 +13,7 @@ module.exports = app => {
     .get(route.path, (req, res) => {
       const jsFiles = ['js/toggle-area.js']
       res.render(name, {
-        ...routeUtils.getViewData(req, { data: req.query, language: "_en" }),
+        ...routeUtils.getViewData(req, { data: req.query, language: `_${i18n.getLocale(req)}` }),
         jsFiles,
       })
     })
--- routes/translations-1/translations-1.controller.js
+++ routes/translations-1/translations-1.controller.js
@@ -1,17 +1,20 @@
 const path = require('path')
 const { routeUtils } = require('./../../utils')
+const i18n = require('i18n')
 
 module.exports = app => {
   const name = 'translations-1'
   const route = routeUtils.getRouteByName(name)
 
   routeUtils.addViewPath(app, path.join(__dirname, './'))
-
+  const otherLanguageDict = {en: "fr", fr: "en"}
   app
     .get(route.path, (req, res) => {
+      const language = i18n.getLocale(req)
       const jsFiles = ['js/toggle-area.js']
       res.render(name, {
-        ...routeUtils.getViewData(req, {}),
+        ...routeUtils.getViewData(req, {otherLanguage: "_fr", language: "_en"}),
+        ...routeUtils.getViewData(req, {otherLanguage: `_${otherLanguageDict[language]}`, language: `_${language}`}),
         jsFiles,
       })
     })
