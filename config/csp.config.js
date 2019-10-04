2301e38a8541ff61167a6a1c01fcd85cb12c9c06
Steve Astels <sastels@gmail.com>
2019-10-04 12:56:59 -0400

    csp rules for GA

--- config/csp.config.js
+++ config/csp.config.js
@@ -1,9 +1,15 @@
 // docs: https://helmetjs.github.io/docs/csp/
 module.exports = {
   defaultSrc: ["'self'"],
-  scriptSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com'],
+  scriptSrc: [
+    "'self'",
+    "'unsafe-inline'",
+    'cdnjs.cloudflare.com',
+    'https://www.googletagmanager.com',
+    'https://www.google-analytics.com',
+  ],
   baseUri: ["'none'"],
   fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-  imgSrc: ["'self'", 'data:'],
+  imgSrc: ["'self'", 'data:', 'https://www.google-analytics.com'],
   styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
 }
