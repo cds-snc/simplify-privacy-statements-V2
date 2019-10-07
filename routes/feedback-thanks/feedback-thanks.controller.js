ff21409e4f8c4b3e8e2a7798adb88ae26b4926a3
Steve Astels <sastels@gmail.com>
2019-10-07 18:35:39 -0400

    move notify to feedback controller

--- routes/feedback-thanks/feedback-thanks.controller.js
+++ routes/feedback-thanks/feedback-thanks.controller.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { getNextRoute, routeUtils } = require('./../../utils')
+const { getNextRoute, routeUtils, sendNotification } = require('./../../utils')
 const { Schema } = require('./schema.js')
 
 module.exports = app => {
@@ -10,7 +10,26 @@ module.exports = app => {
 
   app
     .get(route.path, (req, res) => {
-      res.render(name, { ...routeUtils.getViewData(req, {}), nextRoute: getNextRoute(name).path })
+      const data = routeUtils.getViewData(req, {}).data
+      console.log({ data })
+
+      try {
+        sendNotification({
+          email: process.env.FEEDBACK_EMAIL_ADDRESS,
+          templateId: process.env.TEMPLATE_ID,
+          options: {
+            personalisation: {
+              feedback: data.what_went_wrong,
+            },
+          },
+        })
+      } catch (err) {
+        console.log(`Error: ${err}`)
+      }
+      res.render(name, {
+        ...routeUtils.getViewData(req, {}),
+        nextRoute: getNextRoute(name).path,
+      })
     })
     .post(route.path, [
       ...routeUtils.getDefaultMiddleware({ schema: Schema, name: name }),
