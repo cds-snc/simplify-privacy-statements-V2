28c2ede75084a30438a6da14758ce254d0d9f1f4
Steve Astels <sastels@gmail.com>
2019-10-10 15:33:13 -0400

    only send emails to govt addresses

--- routes/questions-1/schema.js
+++ routes/questions-1/schema.js
@@ -3,7 +3,10 @@ const Schema = {
     custom: {
       options: (value, { req }) => {
         const confidentiality = req.body.confidentiality
-        if (confidentiality && ['form.anonymized', 'form.anonymous'].includes(confidentiality)) {
+        if (
+          confidentiality &&
+          ['form.anonymized', 'form.anonymous'].includes(confidentiality)
+        ) {
           if (value === 'Video and audio') {
             return false
           }
@@ -17,7 +20,7 @@ const Schema = {
     custom: {
       options: (value, { req }) => {
         const compensation = req.body.compensation
-        if (compensation && compensation === "Yes") {
+        if (compensation && compensation === 'Yes') {
           if (value !== 'On paper') {
             return false
           }
@@ -25,6 +28,19 @@ const Schema = {
         return true
       },
       errorMessage: 'errors.consent',
+    },
+  },
+  researcher_email: {
+    custom: {
+      options: (value, { req }) => {
+        if (value && value.length > 0) {
+          if (!(value.endsWith('@cds-snc.ca') || value.endsWith('.gc.ca'))) {
+            return false
+          }
+        }
+        return true
+      },
+      errorMessage: 'errors.researcher_email',
     },
   },
 }
