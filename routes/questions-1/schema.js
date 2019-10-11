39d0ffb856a8ddf5bad7fd2c6ba9628b4481a107
Steve Astels <sastels@users.noreply.github.com>
2019-10-11 09:11:47 -0400

    make some fields mandatory (#101)

--- routes/questions-1/schema.js
+++ routes/questions-1/schema.js
@@ -1,9 +1,40 @@
+const requiredFor = language => {
+  return {
+    custom: {
+      options: (value, { req }) => {
+        if (
+          process.env.NODE_ENV !== 'development' &&
+          req.body.language === language &&
+          !(value && value.length > 0)
+        ) {
+          return false
+        }
+
+        return true
+      },
+      errorMessage: 'errors.requiredField',
+    },
+  }
+}
+
 const Schema = {
+  research_goal_en: requiredFor('_en'),
+  research_goal_fr: requiredFor('_fr'),
+  session_activity_en: requiredFor('_en'),
+  session_activity_fr: requiredFor('_fr'),
+  session_duration_en: requiredFor('_en'),
+  session_duration_fr: requiredFor('_fr'),
+  personal_information_collected_en: requiredFor('_en'),
+  personal_information_collected_fr: requiredFor('_fr'),
+
   recording_type: {
     custom: {
       options: (value, { req }) => {
         const confidentiality = req.body.confidentiality
-        if (confidentiality && ['form.anonymized', 'form.anonymous'].includes(confidentiality)) {
+        if (
+          confidentiality &&
+          ['form.anonymized', 'form.anonymous'].includes(confidentiality)
+        ) {
           if (value === 'Video and audio') {
             return false
           }
@@ -17,7 +48,7 @@ const Schema = {
     custom: {
       options: (value, { req }) => {
         const compensation = req.body.compensation
-        if (compensation && compensation === "Yes") {
+        if (compensation && compensation === 'Yes') {
           if (value !== 'On paper') {
             return false
           }
