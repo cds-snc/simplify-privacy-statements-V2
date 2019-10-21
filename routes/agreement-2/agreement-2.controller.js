48e21ddb2201da31c13e6e6e5debf43b2e2973f2
Steve Astels <sastels@gmail.com>
2019-10-21 08:52:11 -0400

    remove _ens and clean up code

--- routes/agreement-2/agreement-2.controller.js
+++ routes/agreement-2/agreement-2.controller.js
@@ -1,31 +1,6 @@
 const path = require('path')
 const { getNextRoute, routeUtils } = require('./../../utils')
-const nodePandoc = require('node-pandoc')
 const i18n = require('i18n')
-const url = require('url')
-
-var callback = (err, result) => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log('done conversion')
-  }
-}
-
-function getRandomString() {
-  return Math.random()
-    .toString()
-    .split('.')[1]
-    .slice(0, 8)
-}
-
-// make first letter lowercase and delete trailing periods
-const toPhrase = s =>
-  s && s.length > 0 ? (s[0].toLowerCase() + s.slice(1)).replace(/\.+$/, '') : s
-
-const eligibleKey = key =>
-  (key.includes('_en') || key.includes('_fr')) &&
-  !key.includes('partner_department')
 
 module.exports = app => {
   const name = 'agreement-2'
@@ -35,46 +10,17 @@ module.exports = app => {
 
   app.get(route.path, (req, res) => {
     var nextRoute = getNextRoute(name).path
-    var randomString = getRandomString()
-    var docxFilename = 'agreement-' + randomString + '.docx'
-
-    const data = routeUtils.getViewData(req, {}).data
-    var queryParams = {}
-    Object.keys(data)
-      .filter(key => key !== '_csrf' && data[`${key}`] !== '')
-      .forEach(key => {
-        if (eligibleKey(key)) {
-          data[`${key}`] = toPhrase(data[`${key}`])
-        }
-        queryParams[`${key}`] = data[`${key}`]
-      })
-    const link = url.format({
-      protocol: req.protocol,
-      host: req.get('Host'),
-      pathname: routeUtils.getRouteByName('questions-2').path,
-      query: queryParams,
-    })
-    console.log(`length of link: ${link.length}`)
 
     res.render(
       name + `-${i18n.getLocale(req)}`,
       {
-        ...routeUtils.getViewData(req, { data: { link } }),
+        ...routeUtils.getViewData(req, {}),
         nextRoute: nextRoute,
-        docxFilename: docxFilename,
       },
       function(err, html) {
         if (err) {
           console.log(err)
         }
-        const startIndex = html.indexOf('<h1>')
-        const endIndex = html.indexOf('</main>')
-        const htmlDoc = html.slice(startIndex, endIndex)
-        nodePandoc(
-          htmlDoc,
-          '-f html -t docx -o public/documents/' + docxFilename,
-          callback,
-        )
         res.send(html)
       },
     )
