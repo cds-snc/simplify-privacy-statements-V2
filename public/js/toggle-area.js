38d71d4b5ea1d1ae9d2c91a25c2f8f9c0618f3ad
Steve Astels <sastels@users.noreply.github.com>
2019-10-24 19:03:38 -0400

    Get hidden questions working on IE / Edge (#131)

--- public/js/toggle-area.js
+++ public/js/toggle-area.js
@@ -1,25 +1,25 @@
 // generic show hide toggle
 // show or hide a div (more form fields) when clicked etc...
 
-const toggleArea = event => {
+function toggleArea(event) {
   // ideally this won't be based on the index but....
   // we need to adjust the radio macro so we can set attributes
   // for individual inputs
   // current { 'on':'Yes','off':'No'}
   // - future {attributes : {data-something: "something"}, values: { '1':'Yes','2':'No'}}
-  const toggleState = event.target.getAttribute('data-index')
-  const name = event.target.name
-  const checked = event.target.checked
+  var toggleState = event.target.getAttribute('data-index')
+  var name = event.target.name
+  var checked = event.target.checked
 
   // look for an element with that name of the target + -toggled
   // i.e. div you want to show or hide
-  const target = document.querySelector(`.${name}-toggled`)
+  var target = document.querySelector('.' + name + '-toggled')
   if (!checked || !target) return
 
   if (!target.classList.contains('form')) {
     target.classList.add('form')
   }
-  if (toggleState.includes('on')) {
+  if (toggleState.indexOf('on') >= 0) {
     target.style.display = 'block'
   } else {
     target.style.display = 'none'
@@ -27,9 +27,15 @@ const toggleArea = event => {
 }
 
 // add an event listener for all inputs within a toggle area
-const toggles = document.querySelectorAll('.toggle-area input')
+var toggles = document.querySelectorAll('.toggle-area input')
 
-toggles.forEach(el => {
-  toggleArea({ target: el })
+for (var i = 0; i < toggles.length; i++) {
+  // eslint-disable-next-line security/detect-object-injection
+  var el = toggles[i]
   el.addEventListener('click', toggleArea)
-})
+}
+
+// toggles.forEach(function toggle(el) {
+//   toggleArea({ target: el })
+//   el.addEventListener('click', toggleArea)
+// })
