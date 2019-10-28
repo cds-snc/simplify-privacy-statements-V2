834c33466823f7705d7d7bab47157cc9fcf3e95e
Stephen McMurtry <smcmurtry@gmail.com>
2019-10-28 16:08:58 -0600

    client js should now load on heroku hosted apps (#111)

--- config/csp.config.js
+++ config/csp.config.js
@@ -1,6 +1,6 @@
 // docs: https://helmetjs.github.io/docs/csp/
 
-const scriptSrc = ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com']
+const scriptSrc = ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', '*.herokuapp.com']
 
 if (process.env.NODE_ENV === 'development') {
   scriptSrc.push("'unsafe-eval'")
@@ -8,9 +8,10 @@ if (process.env.NODE_ENV === 'development') {
 
 module.exports = {
   defaultSrc: ["'self'"],
-  scriptSrc,
+  scriptSrc: scriptSrc,
   baseUri: ["'none'"],
   fontSrc: ["'self'", 'https://fonts.gstatic.com'],
   imgSrc: ["'self'", 'data:'],
   styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+  upgradeInsecureRequests: true,
 }
