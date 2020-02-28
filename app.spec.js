038c6595ff1c71a4da1c7b3a2329ebc0f681b826
smcmurtry <smcmurtry@gmail.com>
2020-02-28 12:09:56 -0700

    fixed tests

--- app.spec.js
+++ app.spec.js
@@ -1,4 +1,4 @@
-let { routes } = require('./config/routes.config')
+const { routes } = require('./config/routes.config')
 const request = require('supertest')
 const app = require('./app.js')
 
@@ -11,6 +11,6 @@ jest.mock('./config/routes.config', () => {
 })
 
 test('Server can request first route and receive 200 response', async () => {
-  const response = await request(app).get(routes[0].path.en)
-  expect(response.statusCode).toBe(200)
+  const response = await request(app).get(routes[0].path)
+  expect(response.statusCode).toBe(302)
 })
--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -1,11 +1,10 @@
 const request = require('supertest')
 const app = require('../../app.js')
-const { getRouteByName } = require('../../utils/route.helpers')
 jest.mock("node-pandoc", () => () => null);
 
 test('Can send get request agreement-1 route ', async () => {
-  const route = getRouteByName('agreement-1')
-  const response = await request(app).get(route.path)
+  const route = app.routes.get('agreement-1')
+  const response = await request(app).get(route.path.en)
   expect(response.statusCode).toBe(200)
 })
 
