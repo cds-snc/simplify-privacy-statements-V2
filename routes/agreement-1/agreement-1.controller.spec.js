485081b5da97845c4805d4888f9038d80cc7e947
omartehsin1 <omartehsin@gmail.com>
2023-03-13 11:57:56 -0400

    fixed linting issue

--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -7,7 +7,7 @@ test('Can send get request agreement-1 route ', async () => {
   const route = app.routes.get('agreement-1')
   
   jest.fn().mockImplementation(() => {
-    let response = request(app).get(route.path.en)
+    const response = request(app).get(route.path.en)
     console.log('my response', response)
     expect(response.statusCode).toBe(200)
   })
