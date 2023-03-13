fe226da9a1933b1315fd8368fa453bf0432c5981
omartehsin1 <omartehsin@gmail.com>
2023-03-13 11:51:02 -0400

    fixed issue

--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -5,7 +5,10 @@ jest.mock("node-pandoc", () => () => null);
 
 test('Can send get request agreement-1 route ', async () => {
   const route = app.routes.get('agreement-1')
-  jest.fn().mockImplementation((cb) => cb(() =>{
+  jest.fn().mockImplementation((cb) => cb((error) => {
+    if (error) {
+      console.log(error)
+    }
     const response = request(app).get(route.path.en)
     expect(response.statusCode).toBe(200)
   }))
