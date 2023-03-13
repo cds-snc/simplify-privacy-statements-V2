b731ba82c227e9c9e1e0a542c519927fe8ef8640
omartehsin1 <omartehsin@gmail.com>
2023-03-13 11:45:48 -0400

    fixed unit test

--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -1,27 +1,12 @@
 const request = require('supertest')
 const app = require('../../app.js')
-// const nodePandoc = require('node-pandoc')
-jest.mock("node-pandoc", () => () => null)
-// jest.mock("nodePandoc")
+jest.mock("node-pandoc", () => () => null);
 
-test('Can see if I get a response', async () => {
-  // jest.mock("node-pandoc", () => () => null)
+
+test('Can send get request agreement-1 route ', async () => {
   const route = app.routes.get('agreement-1')
-  const response = await request(app).get(route.path.en)
-  console.log("response is: ", response.statusCode)
-  // console.log(route.path.en)
-  
-  expect("hello").toBe("hello")
+  jest.fn().mockImplementation((cb) => cb(() =>{
+    const response = request(app).get(route.path.en)
+    expect(response.statusCode).toBe(200)
+  }))
 })
-
-// test('Can see if response', async () => {
-//   const route = app.routes.get('agreement-1')
-//   request(app).get(route.path.en).expect(200)
-// })
-
-// test('Can send get request agreement-1 route ', async () => {
-//   const route = app.routes.get('agreement-1')
-//   const response = await request(app).get(route.path.en)
-//   expect(response.statusCode).toBe(200)
-// })
-
