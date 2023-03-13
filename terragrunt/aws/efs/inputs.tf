53f934fdd3e2b9c9ef4efbfa763b551aeaf79cbc
omartehsin1 <omartehsin@gmail.com>
2023-03-13 17:22:25 -0400

    updated efs

--- terragrunt/aws/efs/inputs.tf
+++ terragrunt/aws/efs/inputs.tf
@@ -1,6 +1,7 @@
 variable "aws_security_group_ids" {
   description = "AWS security group ID used by the EFS mount target."
-  type        = string
+  type        = list(string)
+  default     = [""]
 }
 
 variable "public_subnets_ids" {
