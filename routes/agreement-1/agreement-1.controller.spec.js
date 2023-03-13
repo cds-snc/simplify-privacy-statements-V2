df5048baf285b376f968b7b85b3fc00643cd4b44
omartehsin1 <omartehsin@gmail.com>
2023-03-13 11:51:58 -0400

    fix linting

--- routes/agreement-1/agreement-1.controller.spec.js
+++ routes/agreement-1/agreement-1.controller.spec.js
@@ -5,11 +5,12 @@ jest.mock("node-pandoc", () => () => null);
 
 test('Can send get request agreement-1 route ', async () => {
   const route = app.routes.get('agreement-1')
+  let response;
   jest.fn().mockImplementation((cb) => cb((error) => {
     if (error) {
       console.log(error)
     }
-    const response = request(app).get(route.path.en)
+    response = request(app).get(route.path.en)
     expect(response.statusCode).toBe(200)
   }))
 })
