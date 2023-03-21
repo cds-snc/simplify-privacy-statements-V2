63765dba21e3f245f4376d4609e155a25d77890f
omartehsin1 <omartehsin@gmail.com>
2023-03-21 12:42:13 -0400

    terraform fmt

--- terragrunt/aws/efs/outputs.tf
+++ terragrunt/aws/efs/outputs.tf
@@ -5,5 +5,5 @@ output "aws_efs_access_point" {
 
 output "aws_efs_file_system" {
   description = "EFS File System"
-  value = aws_efs_file_system.generated_statement_efs.arn
+  value       = aws_efs_file_system.generated_statement_efs.arn
 }
