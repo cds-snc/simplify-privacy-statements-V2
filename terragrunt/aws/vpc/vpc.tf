adcb97138376d9df5a4aa7b325d68e6e13a5c307
omartehsin1 <omartehsin@gmail.com>
2023-03-23 10:10:36 -0400

    added egress rule

--- terragrunt/aws/vpc/vpc.tf
+++ terragrunt/aws/vpc/vpc.tf
@@ -30,3 +30,13 @@ resource "aws_security_group_rule" "efs_ingress" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "efs_egress" {
+  description       = "Allows outbound connections to the internet"
+  type              = "egress"
+  security_group_id = aws_security_group.efs_access_sg.id
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
