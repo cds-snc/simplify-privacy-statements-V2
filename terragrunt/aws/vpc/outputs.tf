8957533f3b10db38e578293882a6730bdf04334e
omartehsin1 <omartehsin@gmail.com>
2023-03-28 16:03:02 -0400

    added new sg variables

--- terragrunt/aws/vpc/outputs.tf
+++ terragrunt/aws/vpc/outputs.tf
@@ -12,3 +12,13 @@ output "private_subnet_ids" {
   description = "AWS Public Subnet ID used by the EFS mount target."
   value       = module.vpc.private_subnet_ids
 }
+
+output "lambda_aws_security_group_ids" {
+  description = "AWS Lambda security group ID used by the EFS mount target."
+  value       = aws_security_group.lambda_access_sg.id
+}
+
+output "efs_aws_security_group_ids" {
+  description = "AWS EFS security group ID used by the EFS mount target."
+  value       = aws_security_group.efs_access_sg.id
+}
