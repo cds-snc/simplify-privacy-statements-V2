0f74b47767716c3a08aa67be5f7e0f20727e8e77
omartehsin1 <omartehsin@gmail.com>
2023-04-06 15:34:37 -0400

    csp issue

--- config/csp.config.js
+++ config/csp.config.js
@@ -1,6 +1,6 @@
 // docs: https://helmetjs.github.io/docs/csp/
 
-const scriptSrc = ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', '*.herokuapp.com', 'https://www.googletagmanager.com', 'https://www.google-analytics.com', '*.lambda-url.ca-central-1.on.aws/dist/js/*']
+const scriptSrc = ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com', '*.herokuapp.com', 'https://www.googletagmanager.com', 'https://www.google-analytics.com', 'https://edbi6zcop5ta2t5wb6hb5y6kja0rsajp.lambda-url.ca-central-1.on.aws/dist/js/questions-1.3e7d142ac08d78a0424a.js']
 
 if (process.env.NODE_ENV === 'development') {
   scriptSrc.push("'unsafe-eval'")
