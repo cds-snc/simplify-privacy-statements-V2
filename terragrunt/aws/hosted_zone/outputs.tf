9edadb40a191ee4b924eb86f07ecb9536cd16fe3
omartehsin1 <omartehsin@gmail.com>
2023-04-21 14:27:18 -0400

    updated output

--- terragrunt/aws/hosted_zone/outputs.tf
+++ terragrunt/aws/hosted_zone/outputs.tf
@@ -4,6 +4,6 @@ output "hosted_zone_id" {
 }
 
 output "hosted_zone_id_fr" {
-  description = "Route53 hosted zone ID that will hold our DNS records"
+  description = "Route53 hosted zone ID that will hold our French DNS records"
   value       = aws_route53_zone.simplify_privacy_statements_fr.zone_id
 }
