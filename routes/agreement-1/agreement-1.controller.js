01bcd4525a364be048ce842ee42a67f13eed2410
Cheyenne Arrowsmith <cheyenne.arrowsmith@cds-snc.ca>
2023-08-01 15:50:55 -0400

    tweaked layout and templates

--- routes/agreement-1/agreement-1.controller.js
+++ routes/agreement-1/agreement-1.controller.js
@@ -70,13 +70,13 @@ module.exports = (app, route) => {
 
         // Send the HTML response in the nodePandoc callback to give pandoc time to generate the docx file
         // as the Lambda function will be terminated once it sends the response.
+        // COMMENT THE nodePandoc CALL OUT WHEN TESTING LOCALLY AND MOVE res.send(html) out of nodePandoc call into parent function
         nodePandoc(htmlDoc, '-f html -t docx -o /mnt/access/' + docxFilename, (err) => {
           if(err){
             console.error(`Error from pandoc: ${err}`);
           }
           res.send(html)
         })
-                
       },
     )
   })
